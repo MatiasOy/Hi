262a075c7825c8a15392db730441cc6365c05217
MatiasOy <77890984+MatiasOy@users.noreply.github.com>
2021-02-04 16:12:13 -0500

    Update Hi.js

--- Hi.js
+++ Hi.js
@@ -1,2 +1,7 @@
 let name = 'Matias'
-const question `hello` ${name}, `how is your day?'
+const question = `hello ${name}, how is your day?`;
+const weather = ['sunny', 'rainy', 'humid']
+const day_of_week = {
+    day: 'Monday',
+    time: 2:30
+}
